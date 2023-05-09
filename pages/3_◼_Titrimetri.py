c02305dfdd63f16a2b30162c839462480eff767b
Adrianchdra <130055928+Adrianchdra@users.noreply.github.com>
2023-05-09 09:41:24 +0700

    Update 3_◼_Titrimetri.py

--- pages/3_◼_Titrimetri.py
+++ pages/3_◼_Titrimetri.py
@@ -3,20 +3,15 @@ import streamlit as st
 st.title("◼ ANALISIS TITRIMETRI")
 st.write("Perhatian : Gunakan konsentrasi titran dalam grek/L")
 
-Bobot = st.number_input("Masukan Bobot Sampel, dalam satuan gram")
+VolumSampel = st.number_input("Masukan Volume atau Bobot Sampel, dalam satuan mL atau gram")
 Volume = st.number_input("Masukan Volume Titran, dalam satuan mililiter")
 Konsentrasi = st.number_input("Masukan Konsentrasi Titran")
-BEBM = st.number_input("Masukan BE atau BM,")
+BEBM = st.number_input("Masukan BE atau BM")
 FP = st.number_input("Masukan Faktor Pengali atau Pengenceran")
 KS = st.number_input("Masukan Kadar Sebenarnya")
 
 Kadar = st.button("KADAR")
-Kesalahan = st.button("KESALAHAN")
 
 if Kadar:
-    Kadar = ((Volume*Konsentrasi*BEBM*FP)/1000*100)/Bobot
+    Kadar = ((Volume*Konsentrasi*BEBM*FP)/1000*100)/VolumSampel
     st.success(f"{Kadar} %")
-
-if Kesalahan:
-    Kesalahan = ((KS-Kadar)/KS)*100
-    st.success(f"{Kesalahan} %")
