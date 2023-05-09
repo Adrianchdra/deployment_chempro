29d7a62cff643ab88b8f97f0fee56ee2957eb4f2
Adrianchdra <130055928+Adrianchdra@users.noreply.github.com>
2023-05-09 09:34:52 +0700

    Update 8_📖_About_ChemPro.py

--- pages/8_📖_About_ChemPro.py
+++ pages/8_📖_About_ChemPro.py
@@ -4,9 +4,9 @@ from PIL import Image
 st.title("Welcome to ChemPro")
 st.text("Your Chemical Support")
 
-img = Image.open("D:\ProjectLPK\Clone\ChemPro.jpg")
+img = Image.open("ChemPro.jpg")
 
-st.image(img, width=500)
+st.image(img)
 
 st.write("ChemPro adalah aplikasi berbasis web yang dirancang untuk membantu anda dalam bidang analisis kimia. ChemPro menyediakan berbagai fitur yang utama dalam bidang analisis kimia. ChemPro dapat anda akses dimanapun dan kapanpun melalui gadget anda. ChemPro hadir untuk membantu. ChemPro, your chemical support")
 st.text("Akses ChemPro secara gratis sekarang !")
